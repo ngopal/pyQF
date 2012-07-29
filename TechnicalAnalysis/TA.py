d6c4ddd4656305fd5a6273649d1d2d35d66419ae
Nikhil Gopal <nikhilgopal@gmail.com>
2012-07-28 17:47:37 -0700

    started working on master class to make driving analysis easier

---
+++ TechnicalAnalysis/TA.py
@@ -0,0 +1,6 @@
+
+
+class Analyze:
+	def __init__(self,dataObj):
+		print 'This will be the master class to drive the use of the analysis classes'
+
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from Data import Present
 from Data import Historical
-from TechnicalAnalysis import *
+from TechnicalAnalysis import TA
 
 print Present.get_all('GOOG')
 H = Historical.Quotes('GOOG',2004,2010)
 print H.data
+T = TA.Analyze(H) #written as H, but maybe better to take H.data explicitly
